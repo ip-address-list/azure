7ce3c6ecebfbcc92b8f7cfde2b160c71e2a7ccdb
Jim Liu <jim-liu@users.noreply.github.com>
2025-01-30 14:31:27 +0800

    Migrate to .NET 8. Update Azure geographies.

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
-using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace azure
 {
@@ -12,10 +12,7 @@ namespace azure
     {
         static void Main(string[] args)
         {
-            // United States geography has the following regions:
-            // Central US, East US, East US 2, North Central US, 
-            // South Central US, West Central US, West US, West US 2
-            // This list is accurate as of 8/26/2021
+            // Azure Geographies in the United States, as of January 2025.
             List<string> USGeographyRegions = new List<string>
             {
                 "centralus",
@@ -25,45 +22,42 @@ namespace azure
                 "southcentralus",
                 "westcentralus",
                 "westus",
-                "westus2"
+                "westus2",
+                "westus3"
             };
 
-            // Load the weekly file
             var today = DateTime.UtcNow;
             var monday = today.AddDays(DayOfWeek.Monday - today.DayOfWeek);
             var url = @"https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_" + monday.ToString("yyyyMMdd") + ".json";
             var fileName = monday.ToString("yyyyMMdd") + ".json";
-            var client = new WebClient();
-            Console.WriteLine("Download " + url + " to " + fileName + ".");
-            client.DownloadFile(url, fileName);
-            JObject weeklyFile = JObject.Parse(File.ReadAllText(fileName));
-            JArray values = (JArray)weeklyFile["values"];
+            var client = new HttpClient();
+            var jsonDoc = JsonDocument.Parse(client.GetStringAsync(url).Result);
+            var values = jsonDoc.RootElement.GetProperty("values");
 
-            var ipAddresses = new List<String>();
+            var ipAddresses = new List<string>();
 
             foreach (string region in USGeographyRegions)
             {
                 string azureCloudRegion = $"AzureCloud.{region}";
 
-                var ipList =
-                    from v in values
-                    where (string)v["name"] == azureCloudRegion
-                    select v["properties"]["addressPrefixes"];
+                var ipList = values.EnumerateArray()
+                    .Where(v => v.GetProperty("name").GetString() == azureCloudRegion)
+                    .SelectMany(v => v.GetProperty("properties").GetProperty("addressPrefixes").EnumerateArray());
 
-                foreach (var ip in ipList.Children())
+                foreach (var ip in ipList)
                 {
-                    ipAddresses.Add(ip.ToString());
+                    ipAddresses.Add(ip.GetString());
                 }
             }
 
-            System.IO.File.WriteAllText(@"azure-us.txt", String.Join(Environment.NewLine, ipAddresses));
-            System.IO.File.WriteAllText(@"azure-us-v4.txt", String.Join(Environment.NewLine, ipAddresses.Where(ip => ip.IndexOf(":") < 0)));
-            System.IO.File.WriteAllText(@"azure-us-v6.txt", String.Join(Environment.NewLine, ipAddresses.Where(ip => ip.IndexOf(":") > 0)));
-            System.IO.File.WriteAllText(@"azure-us.csv", String.Join(",", ipAddresses));
-            System.IO.File.WriteAllText(@"azure-us-v4.csv", String.Join(",", ipAddresses.Where(ip => ip.IndexOf(":") < 0)));
-            System.IO.File.WriteAllText(@"azure-us-v6.csv", String.Join(",", ipAddresses.Where(ip => ip.IndexOf(":") > 0)));
+            File.WriteAllText(@"azure-us.txt", string.Join(Environment.NewLine, ipAddresses));
+            File.WriteAllText(@"azure-us-v4.txt", string.Join(Environment.NewLine, ipAddresses.Where(ip => !ip.Contains(":"))));
+            File.WriteAllText(@"azure-us-v6.txt", string.Join(Environment.NewLine, ipAddresses.Where(ip => ip.Contains(":"))));
+            File.WriteAllText(@"azure-us.csv", string.Join(",", ipAddresses));
+            File.WriteAllText(@"azure-us-v4.csv", string.Join(",", ipAddresses.Where(ip => !ip.Contains(":"))));
+            File.WriteAllText(@"azure-us-v6.csv", string.Join(",", ipAddresses.Where(ip => ip.Contains(":"))));
 
-            System.IO.File.Delete(fileName);
+            File.Delete(fileName);
         }
     }
 }
